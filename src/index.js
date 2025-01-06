dcc9e738bf0657bd6da8c9ab5968f01fc56cb71f
Isabelle <belleyuri@gmail.com>
2025-01-06 16:15:55 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/global.css'; // 전역 스타일 임포트
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
